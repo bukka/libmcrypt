8ddff38affdceddff1090c9de52a34e2b2f4e5d4
imipak <imipak>
2003-09-22 20:55:59 +0000

    *** empty log message ***

--- assemble.sh
+++ assemble.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
-rm -f modules.build
-touch modules.build
+
+cat output-starting.src > lastline.src
+
+MODULES=`cat output-starting.src`
+
 for i in secretkey publickey
 do
   for j in block stream
   do
-    cat modules/$i/$j/list.txt >> modules.build
+    for k in `cat modules/$i/$j/list.txt`
+    do
+      MODULES="$MODULES modules/$i/$j/$k/Makefile"
+    done
   done
 done 
+
+
+MODULES="$MODULES `cat output-ending.src`"
+
+echo $MODULES
+
+cat configure.in.src > configure.in
+
+echo $MODULES >> configure.in
 
