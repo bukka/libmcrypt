1dbdac5f27e7a80779b8d0f0b6e37f255460b4cb
Jakub Zelenka <bukka@php.net>
2015-08-17 20:27:51 +0100

    Move CBC mode code to the standalone module

--- assemble.sh
+++ assemble.sh
@@ -2,19 +2,25 @@
 
 cat output-starting.src > lastline.src
 
-MODULES=`cat output-starting.src`
+#MODULES=`cat output-starting.src`
 
-for i in secretkey publickey
-do
-  for j in block stream
-  do
-    for k in `cat modules/$i/$j/list.txt`
-    do
-      MODULES="$MODULES modules/$i/$j/$k/Makefile"
-    done
-  done
-done 
+#for i in secretkey publickey
+#do
+#  for j in block stream
+#  do
+#    for k in `cat modules/$i/$j/list.txt`
+#    do
+#      MODULES="$MODULES modules/$i/$j/$k/Makefile"
+#    done
+#  done
+#done 
 
+# temporary list
+MODULES="AC_OUTPUT([Makefile libmcrypt.spec lib/Makefile modules/Makefile \
+modules/secretkey/Makefile modules/secretkey/modes/Makefile \
+modules/secretkey/modes/cbc/Makefile \
+modules/secretkey/block/Makefile \
+modules/secretkey/block/3-way/Makefile"
 
 MODULES="$MODULES `cat output-ending.src`"
 
